01227302691984f5efee84ce94a92b8b022bd38d
Andre <ajparker000@gmail.com>
2020-06-02 16:38:15 -0700

    updated app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CardList from './CardList';
+import Searchbox from './Searchbox';
+import Scroll from './Scroll';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { Robots: [], searchfield: '' };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState({ Robots: users }));
+  }
+
+  onSearchchange = (e) => {
+    this.setState({ searchfield: e.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.Robots.filter((robot) => {
+      return robot.name
+        .toLocaleLowerCase()
+        .includes(this.state.searchfield.toLocaleLowerCase());
+    });
+    if (this.state.Robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f1 light-gray'>RoboFriends</h1>
+          <Searchbox searchChange={this.onSearchchange} />
+          <Scroll>
+            <CardList Robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
---
+++ src/Robots.js
@@ -0,0 +1,38 @@
+export const Robots = [
+  {
+    id: 1,
+    name: 'Dre',
+    username: 'J',
+    email: 'Drej@gmail.com',
+  },
+  {
+    id: 2,
+    name: 'Jane',
+    username: 'C',
+    email: 'Janec@gmail.com',
+  },
+  {
+    id: 3,
+    name: 'Ree',
+    username: 'M',
+    email: 'Ree@gmail.com',
+  },
+  {
+    id: 4,
+    name: 'Dre',
+    username: 'J',
+    email: 'Drej@gmail.com',
+  },
+  {
+    id: 5,
+    name: 'Jane',
+    username: 'C',
+    email: 'Janec@gmail.com',
+  },
+  {
+    id: 6,
+    name: 'Ree',
+    username: 'M',
+    email: 'Ree@gmail.com',
+  },
+];
