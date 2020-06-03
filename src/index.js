ee5fc1d8c0b0186223917761c0c9bf473d0c5ba2
Andre <ajparker000@gmail.com>
2020-06-03 16:40:11 -0700

    added redux middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './App';
 import 'tachyons';
 import { searchRobots } from './reducers';
 
+//Installed logger middleware to help debug our code
+const logger = createLogger();
+
 //Created Redux store and the Provider component that passes down the store to the components
-const store = createStore(searchRobots);
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
